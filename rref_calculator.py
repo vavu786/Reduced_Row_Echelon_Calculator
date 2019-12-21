4f1394c658648e3c3a6c233ed39831546defc8b8
Hamza Patwa <hamzamian.patwa@gmail.com>
2019-12-20 21:03:58 -0600

    Changed formatting to PEP compliant

--- rref_calculator.py
+++ rref_calculator.py
@@ -1,17 +1,18 @@
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#This calculator makes any matrix of dimensions n by n+1 into reduced row echelon form. 
-#Reduced row echelon form is very useful for solving large systems of equations.
-#I used Gauss-Jordan elinination to make this calculator
-#Uses NumPy; custom matrix
-#Includes step by step instructions
-#I have a Java version as well without any matrix library
-#Note: The : means all the entries in that dimension. So inputMatrix[3,:] means the third row.
-#Created by Hamza Patwa (11th grade)
-#Happy reduced row echeloning!
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# This calculator makes any matrix of dimensions n by n+1 into reduced row echelon form. 
+# Reduced row echelon form is very useful for solving large systems of equations.
+# I used Gauss-Jordan elinination to make this calculator
+# Uses NumPy; custom matrix
+# Includes step by step instructions
+# I have a Java version as well without any matrix library
+# Note: The : means all the entries in that dimension. So inputMatrix[3,:] means the third row.
+# Created by Hamza Patwa (11th grade)
+# Happy reduced row echeloning!
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 import numpy as np
 import argparse
 from fractions import Fraction
+
 
 def parse_args():
     parser = argparse.ArgumentParser(prog='PROG')
@@ -19,21 +20,24 @@ def parse_args():
     parser.add_argument('-values', action='store', type=float, nargs='+')
     args = parser.parse_args()
     return args
-     
-#Multiplies a row and a scalar
-def multiplyRow(multiplier, rowNum):
-    inputMatrix[rowNum,:] *= multiplier
-    
-#Adds two rows and replaces that sum into the row inputed last
-def addRows(sourceRow, targetRow):
-    inputMatrix[targetRow,:] += inputMatrix[sourceRow,:]
 
-#Checks a certain column if it is in RREF form
+
+# Multiplies a row and a scalar
+def multiplyRow(multiplier, rowNum):
+    inputMatrix[rowNum, :] *= multiplier
+  
+
+# Adds two rows and replaces that sum into the row inputed last
+def addRows(sourceRow, targetRow):
+    inputMatrix[targetRow, :] += inputMatrix[sourceRow,:]
+
+
+# Checks a certain column if it is in RREF form
 def checkValueRREF(row, column):
-    #Makes a column matrix equal to the correct RREF form it should be 
+    # Makes a column matrix equal to the correct RREF form it should be 
     RREFCol = np.empty ([NUMROWS, 1], dtype = 'float')
-    #Makes a column matrix equal to the values of inputMatrix in a specific column
-    columnMatrix = np.empty ([NUMROWS, 1], dtype = 'float')
+    # Makes a column matrix equal to the values of inputMatrix in a specific column
+    columnMatrix = np.empty([NUMROWS, 1], dtype = 'float')
     for i in range(NUMROWS):
         columnMatrix[i][0] = inputMatrix[i][column]
         if i == column:
@@ -45,10 +49,11 @@ def checkValueRREF(row, column):
     else:
         return False
 
+
 def checkColRREF(column):
-    #Makes a column matrix equal to the correct RREF form it should be 
+    # Makes a column matrix equal to the correct RREF form it should be 
     RREFCol = np.empty ([NUMROWS, 1], dtype = 'float')
-    #Makes a column matrix equal to the values of inputMatrix in a specific column
+    # Makes a column matrix equal to the values of inputMatrix in a specific column
     columnMatrix = np.empty ([NUMROWS, 1], dtype = 'float')
     for i in range(NUMROWS):
         columnMatrix[i][0] = inputMatrix[i][column]
@@ -60,18 +65,19 @@ def checkColRREF(column):
         return True
     else:
         return False
-    
-#Makes an input column into RREF
+   
+ 
+# Makes an input column into RREF
 def RREF (colNum):
     for i in range(NUMCOLS-1):
-        #Makes the diagonal entry 1 in that column
+        # Makes the diagonal entry 1 in that column
         if inputMatrix[colNum][colNum] != 1.0:
             if (np.count_nonzero(inputMatrix[i,:])) == 0:
                 return "zerorow"
-            #If the diagonal entry is 0
+            # If the diagonal entry is 0
             if inputMatrix[colNum][colNum] == 0:
                 for i in range(NUMCOLS-1):
-                    #If there is any diagonal entry that isn't 0, add the 0 column and that other column
+                    # If there is any diagonal entry that isn't 0, add the 0 column and that other column
                     if inputMatrix[i][colNum] != 0:
                         print("Add row {} to row {} and replace it into row {}: ".format(i, colNum, colNum))
                         addRows(i, colNum)
@@ -79,39 +85,41 @@ def RREF (colNum):
                         break
                 if inputMatrix[colNum][colNum] == 0:
                     return "zerocol"
-            print ("Divide row {} by {}(beginning): ".format(colNum, inputMatrix[colNum][colNum]))
+            print("Divide row {} by {}(beginning): ".format(colNum, inputMatrix[colNum][colNum]))
             multiplyRow(1/inputMatrix[colNum][colNum], colNum)
             print(inputMatrix)
-        #The if statement says that if the [colNum][colNum] is not 1 or any other value is not 0, run the RREF code
-        if checkValueRREF(i, colNum) == False:
-            #Checks if there is a row of zeros
-            if (np.count_nonzero(inputMatrix[i,:])) == 0:
+        # The if statement says that if the [colNum][colNum] is not 1 or any other value is not 0, run the RREF code
+        if not checkValueRREF(i, colNum):
+            # Checks if there is a row of zeros
+            if (np.count_nonzero(inputMatrix[i, :])) == 0:
                 return "zerorow"
             if inputMatrix[colNum][colNum] == -1:
                 multiplyRow(-1, colNum)
-            #Multiplies the row with a 1 by the negative of the entry you are trying to make 0
-            print ("Multiply row {} by {}: ".format(colNum, -inputMatrix[i][colNum]))
+            # Multiplies the row with a 1 by the negative of the entry you are trying to make 0
+            print("Multiply row {} by {}: ".format(colNum, -inputMatrix[i][colNum]))
             multiplyRow(-inputMatrix[i][colNum], colNum)
-            print (inputMatrix)
-            #Adds the two rows, making one entry zero
-            print ("Add row {} and row {} and replace that into row {}: ".format(i, colNum, i))
+            print(inputMatrix)
+            # Adds the two rows, making one entry zero
+            print("Add row {} and row {} and replace that into row {}: ".format(i, colNum, i))
             addRows(colNum, i)
-            print (inputMatrix)
-        #Makes the diagonal entry 1 again
+            print(inputMatrix)
+        # Makes the diagonal entry 1 again
         if inputMatrix[colNum][colNum] != 1.0 and inputMatrix[colNum][colNum] != 0.0:
-            if (np.count_nonzero(inputMatrix[i,:])) == 0:
+            if (np.count_nonzero(inputMatrix[i, :])) == 0:
                 return "zerorow"
-            if ((np.count_nonzero(inputMatrix[i,:])) == 1 and inputMatrix[NUMROWS-1][NUMCOLS-1] != 0):
+            if np.count_nonzero(inputMatrix[i, :] == 1 and inputMatrix[NUMROWS-1][NUMCOLS-1] != 0):
                 return "zerocol"
-            print ("Divide row {} by {}(end): ".format(colNum, inputMatrix[colNum][colNum]))
+            print("Divide row {} by {}(end): ".format(colNum, inputMatrix[colNum][colNum]))
             multiplyRow((1/inputMatrix[colNum][colNum]), colNum)
             print(inputMatrix)
         if i == (NUMCOLS-2):
             return 1
+
+
 def matrixFunction():
     counter = 97
     for i in range(NUMROWS):
-        if (np.count_nonzero(inputMatrix[i,:])) == 0:
+        if (np.count_nonzero(inputMatrix[i, :])) == 0:
             return
         for j in range(NUMCOLS - 1):
             if j != 0:
@@ -121,6 +129,8 @@ def matrixFunction():
             counter = counter + 1
         print("= {}".format(inputMatrix[i][NUMCOLS-1]))
         counter = 97
+
+
 def variablesEqual():
     counter = 97
     for i in range(NUMROWS):
@@ -128,8 +138,10 @@ def variablesEqual():
         counter = counter + 1
     
 
-#Main function where everything is run
+# Main function where everything is run
 def main():
+
+    # TODO pass these into functions as arguments
     global inputMatrix
     global NUMROWS
     global NUMCOLS
@@ -166,7 +178,8 @@ def main():
         print("RREF of your Matrix is: \n{}".format(inputMatrix))
         variablesEqual()
 
-#Calls the main function
+
+# Calls the main function
 if __name__ == "__main__":
     main()
 
